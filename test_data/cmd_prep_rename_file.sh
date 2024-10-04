90afe8dab4770b92cfdd82ae0e21f200948fa56e
Todd N. Wylie <twylie@wustl.edu>
2024-10-04 15:00:33 -0500

    Making small distro changes while testing.

--- test_data/cmd_prep_rename_file.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/sh
-prep_rename_file \
-    --samplemap batch1/Samplemap.csv batch2/Samplemap.csv \
-    --rename-out prep-rename.tsv
--- test_data/cmds.sh
+++ test_data/cmds.sh
@@ -1,4 +1,7 @@
-#!/usr/bin/sh
+prep_rename_file \
+    --samplemap batch1/Samplemap.csv batch2/Samplemap.csv \
+    --rename-out /tmp/test/prep-rename.tsv
+
 merge_fastq \
     --samplemap batch1/Samplemap.csv batch2/Samplemap.csv \
     --outdir /tmp/test \
@@ -11,3 +14,8 @@ sh /tmp/test/__bsub/2_merge_fastq.sh
 sh /tmp/test/__bsub/3_merge_fastq.sh
 
 tree /tmp/test
+
+eval_fastq_counts \
+    --merged-samplemap /tmp/test/merged_samplemap.tsv \
+    --gtac-counts /tmp/test/gtac_read_counts.tsv \
+    --outdir /tmp/test/
